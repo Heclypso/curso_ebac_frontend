8199d034d864d520dbb84f8675346a300287e36b
Gabriel Munhak <102237924+Heclypso@users.noreply.github.com>
2024-01-02 14:17:53 -0300

    Add files via upload

--- Projeto 2/main.js
+++ Projeto 2/main.js
@@ -3,8 +3,8 @@ const imgAprovado = '<img src="./images/aprovado.png" alt="Emoji celebrando" />'
 const imgReprovado = '<img src="./images/reprovado.png" alt="Emoji decepcionado" />';
 const atividades = [];
 const notas = [];
-const spanAprovado = 'span class="resultado aprovado">Aprovado</span>';
-const spanReprovado = 'span class="resultado aprovado">Reprovado</span>';
+const spanAprovado = '<span class="resultado aprovado">Aprovado';
+const spanReprovado = '<span class="resultado reprovado">Reprovado';
 const notaMinima = parseFloat(prompt("Digite a note mínima:"));
 
 let linhas = '';
@@ -12,7 +12,7 @@ let linhas = '';
 form.addEventListener('submit', function (e) {
     e.preventDefault();
 
-    adicionarLinha();
+    adicionaLinha();
     atualizaTabela();
     atualizaMediaFinal();
 });
@@ -30,7 +30,7 @@ function adicionaLinha() {
         let linha = '<tr>';
         linha += `<td>${inputNomeAtividade.value}</td>`;
         linha += `<td>${inputNotaAtividade.value}</td>`;
-        linha += `<td>${inputNotaAtividade.value >= notaMinima ? imgAprovado : imgReprovado}</td>`;
+        linha += `<td>${parseFloat(inputNotaAtividade.value) >= notaMinima ? imgAprovado : imgReprovado}</td>`;
         linha += '</tr>';
     
         linhas += linha;
@@ -48,14 +48,14 @@ function atualizaTabela() {
 function atualizaMediaFinal(){
     const mediaFinal = calculaMediaFinal();
 
-    document.getElementById('media-final-valor').innerHTML = mediaFinal;
+    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2);
     document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
 }
 
 function calculaMediaFinal(){
     let somaDasNotas = 0;
 
-    for (let i = 0; i < notas.lenght; i++) {
+    for (let i = 0; i < notas.length; i++) {
         somaDasNotas += notas[i];
 
     }
