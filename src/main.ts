5846e668daaf8900914443e08c7d9cd9c2af17e6
Gabriel Munhak <102237924+Heclypso@users.noreply.github.com>
2024-07-02 21:22:17 -0300

    Add files via upload

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+function mult(primeiroNumero: number, segundoNumero: number): number {
+    return primeiroNumero * segundoNumero;
+}
+
+function dizOlá(nome: string): string {
+    return "Olá " + nome;
+}
+
+console.log(mult(5, 5));
+console.log(dizOlá("Garbriel"));
